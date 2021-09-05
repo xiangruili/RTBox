e3e793220c99ac2a6549a8ce0770c5760e701a6a
xiangruili <xiangrui.li@gmail.com>
2021-09-04 22:19:27 -0400

    Update setup_ftd2xx.sh

--- setup_ftd2xx.sh
+++ setup_ftd2xx.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ "$OSTYPE" == "linux"* ]]; then
-    FTD2XX=libftd2xx.so.1.4.8
+    FTD2XX=`ls ./lib/libftd2xx.so.*`
     LNK=libftd2xx.so
 
     # It seems rule necessary for ftd2xx, dialout necessary for VCP
@@ -10,14 +10,16 @@ if [[ "$OSTYPE" == "linux"* ]]; then
     echo 'SUBSYSTEM=="usb", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6001", MODE="0666"' > $RULE_FILE
     usermod -aG dialout $USER
 
-    echo "Disabling VCP driver"
     BL_FILE=/etc/modprobe.d/blacklist.conf
-    echo '' >> $BL_FILE
-    echo '# disable VCP driver to use ftd2xx driver' >> $BL_FILE
-    echo 'blacklist ftdi_sio' >> $BL_FILE
+    grep ^'blacklist ftdi_sio' $BL_FILE >/dev/null || {
+        echo "Disabling VCP driver"
+        echo '' >> $BL_FILE
+        echo '# disable VCP driver to use ftd2xx driver' >> $BL_FILE
+        echo 'blacklist ftdi_sio' >> $BL_FILE
+    }
 
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    FTD2XX=libftd2xx.1.4.16.dylib
+    FTD2XX=`ls ./lib/libftd2xx.*.dylib`
     LNK=libftd2xx.dylib
 
 else
@@ -27,8 +29,9 @@ fi
 
 LIBDIR=/usr/local/lib
 echo "Copying ftd2xx library to $LIBDIR"
-mkdir -p $LIBDIR && cp ./lib/$FTD2XX $LIBDIR
-chmod 0755 $LIBDIR/$FTD2XX
-ln -sf $LIBDIR/$FTD2XX $LIBDIR/$LNK
+mkdir -p $LIBDIR && cp $FTD2XX $LIBDIR
+FTD2XX=$LIBDIR${FTD2XX:5}
+chmod 0755 $FTD2XX
+ln -sf $FTD2XX $LIBDIR/$LNK
 
 echo "Please reboot to make change effect"
